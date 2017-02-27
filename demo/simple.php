c4dad306168823a27a1b26fc8c73f0fc7738fcb5
Бабичев Максим <maksim.babichev95@gmail.com>
2017-02-27 10:13:10 +0300

    Update simple.php

--- demo/simple.php
+++ demo/simple.php
@@ -5,12 +5,12 @@ include_once dirname(__DIR__) . '/vendor/autoload.php';
 $validator = new \Deimos\Validator\Validator();
 
 $validator->field('login')
-    ->alphaDigits('[A-Za-z0-9А-Яа-я]')
+    ->alphaDigits() // [A-Za-z0-9А-Яа-я]
     ->minLength(3, 'Min length equal 3 symbols')
     ->maxLength(16, 'Max length equal 16 symbols');
 
 $validator->field('password')
-    ->anyWord('[A-Za-z0-9А-Яа-я_]')
+    ->anyWord() // [A-Za-z0-9А-Яа-я_]
     ->minLength(6, 'Min length equal 6 symbols')
     ->maxLength(32, 'Max length equal 32 symbols');
 
